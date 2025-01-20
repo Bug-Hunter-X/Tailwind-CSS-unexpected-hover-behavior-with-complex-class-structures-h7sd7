fc8ebb8aeda10f39e2e7c1caf48f2de7d153ebdc
BugHunter-X <askzenai@gmail.com>
2025-01-20 08:28:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,11 @@
+```javascript
+//Corrected version of the Tailwind directives
+<div class="bg-gray-100 hover:bg-gray-200 p-4 rounded-lg shadow-md transition duration-300 ease-in-out hover:shadow-lg">
+  <div class="relative inline-block">
+    <img class="w-16 h-16 rounded-full object-cover transition duration-300 ease-in-out opacity-100 hover:opacity-75" src="/image.jpg" alt="Image">
+    <span class="absolute inset-y-0 right-0 w-4 h-4 bg-red-500 rounded-full mt-2 mr-2">
+      <span class="sr-only">New</span>
+    </span>
+  </div>
+</div>
+```
